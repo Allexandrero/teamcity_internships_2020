a0b63b1bfd99317161a966ea4eb370857ded0c00
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2020-03-05 14:52:11 +0300

    Add message about PythonException immutability

--- src/main/java/python_integration/PythonException.java
+++ src/main/java/python_integration/PythonException.java
@@ -5,6 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import java.io.File;
 import java.util.List;
 
+/**
+ * Please do not modify me
+ */
 public interface PythonException {
     @NotNull
     String getExceptionType();
